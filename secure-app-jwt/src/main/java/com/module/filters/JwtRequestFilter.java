5b0ff935c709aa06a23a336d3fd6103698d39e97
sheikh-itech <sheikh.itech123@gmail.com>
2025-06-18 10:40:32 +0530

    last commit

--- secure-app-jwt/src/main/java/com/module/filters/JwtRequestFilter.java
+++ secure-app-jwt/src/main/java/com/module/filters/JwtRequestFilter.java
@@ -33,6 +33,13 @@ public class JwtRequestFilter  extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
             throws ServletException, IOException {
 
+    	response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, PATCH, HEAD");
+        response.addHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+        response.addHeader("Access-Control-Expose-Headers", "Access-Control-Allow-Origin, Access-Control-Allow-Credentials");
+        response.addHeader("Access-Control-Allow-Credentials", "true");
+        response.addIntHeader("Access-Control-Max-Age", 10);
+        
         final String authorizationHeader = request.getHeader("Authorization");
 
         String username = null;
